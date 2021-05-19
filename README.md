5e25b8226282eda15ad5a5880b29870def7de726
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-05-19 13:55:04 +0530

    Added little utils to generate and automate python documentation

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Sentiment-Analysis
 
     * I'm just trying to learn sentiment analysis using Naive Bayes Classifier.
-> Check out my Naive Bayes Classifier [repo here](https://github.com/Subhash3/Naive-Bayes-Classifier-Py)
+> Check out my Naive Bayes Classifier Repo [here](https://github.com/Subhash3/Naive-Bayes-Classifier-Py)  
+> Download dataset from [here](https://www.kaggle.com/c/sentiment-analysis-on-movie-reviews/data)
 
 ### To Do
-- [ ] Find datasets.
+- [x] Find datasets.
+- [x] Documentation generation using pdoc3.
+- [x] .docsignore file to ignore some files while documenting.
 - [ ] Preprocess dataset using pandas and re.
 - [ ] Tokenization.
 - [ ] Remove stop words, punctuations and special characters.
---
+++ generate_docs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import os
+
+
+def getDocsIgnoreArr(docsIgnoreFile=".docsignore"):
+    try:
+        with open(docsIgnoreFile) as fp:
+            lines = fp.readlines()
+            filesToIgnore = [line.strip() for line in lines]
+    except FileNotFoundError:
+        filesToIgnore = []
+    except Exception as e:
+        print("Error reading .docignore file", e)
+
+    return set(filesToIgnore)
+
+
+def getAllPyFiles():
+    pyFiles = set()
+    dirname = "."
+
+    for filename in os.listdir(dirname):
+        if os.path.isfile(filename) and filename.endswith('.py'):
+            pyFiles.add(filename)
+    return pyFiles
+
+
+filesToIgnore = getDocsIgnoreArr()
+pyFiles = getAllPyFiles()
+docsDir = "docs/"
+filesToDocument = pyFiles.difference(filesToIgnore)
+
+for filename in filesToDocument:
+    # print(filename)
+    os.system(f"pdoc --html {filename} --force -o {docsDir}")
