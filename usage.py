4b28f9e2973edd12568eb9de63d54f5a1c6c1631
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-05-20 22:46:37 +0530

    Added usage

--- usage.py
+++ usage.py
@@ -3,9 +3,23 @@
 from Text_Classifier import TextClassifier
 
 tc = TextClassifier()
-tc.loadDataset('./train_short.json')
+tc.loadDatasetCsv('./datasets/Gunjan933-github/train_short.csv')
 tc.train()
-sentence = "good"
+# for category in tc.summaryByClass:
+#     for word in tc.summaryByClass[category]:
+#         print('\t', word, tc.summaryByClass[category][word])
+#     print()
+# for category in tc.summaryByClass:
+#     print(category)
+#     print(len(tc.summaryByClass[category]))
 
-prediction = tc.predict(sentence)
-print(prediction)
+tests = ["feeling fine",
+         "I hate you",
+         "I love india!!!"]
+
+# for sentence in tests:
+#     prediction = tc.predict(sentence)
+#     print(prediction)
+#     print()
+
+tc.Test()
